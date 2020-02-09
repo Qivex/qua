c0210752544a3469f7356c3750e8b47e44ce5f2d
Qivex <qivex@web.de>
2020-02-09 14:11:37 +0100

    Fixed test-clutter, reworked formula

--- ui/drawable/button.lua
+++ ui/drawable/button.lua
@@ -9,11 +9,11 @@ local Button = Class:extend{
 	new = function(self, caption, pos, size, txcol, bgcol, action, args)
 		self._box = Box(pos, size, bgcol)
 		-- Calculate position of text
-		local x, y = unpack(self._box:getPos())
-		local w, h = unpack(self._box:getSize())
+		local x, y = self._box:getPos()
+		local w, h = self._box:getSize()
 		local txpos = {
-			x - 1 + math.floor((w - string.len(caption or "") / 2),
-			y - 1 + math.ceil(h / 2)
+			x + math.floor((w - string.len(caption or "")) / 2),
+			y + math.floor((h - 1) / 2)
 		}
 		self._text = Text(caption, txpos, txcol, bgcol)
 		self._action = action
@@ -21,13 +21,6 @@ local Button = Class:extend{
 	end,
 	
 	draw = function(self, monitor)
-		-- Write Text
-		monitor.setCursorPos(
-			self._pox_x - 1 + math.floor((self._width - string.len(self._caption)) / 2),
-			self._pox_y - 1 + math.ceil(self._height / 2)
-		)
-		monitor.write(self._caption)
-		
 		self._box:draw(monitor)
 		self._text:draw(monitor)
 	end,
