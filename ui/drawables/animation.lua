d10109f7223c9d0402ceb3b0030540db41ffb159
Qivex <qivex@web.de>
2020-02-15 18:15:07 +0100

    Added getter for anim-length

--- ui/drawables/animation.lua
+++ ui/drawables/animation.lua
@@ -7,11 +7,13 @@ local Animation = Drawable:extend{
 	new = function(self, loop)
 		self._loop = loop
 		self._animation = {}
+		self._current = 1
 	end,
 	
 	draw = function(self, monitor)
 		local drawable = table.remove(self._animation, 1)
 		drawable:draw(monitor)
+		-- Add frame back to end if looped
 		if self._loop then
 			self:addFrame(drawable)
 		end
@@ -19,6 +21,10 @@ local Animation = Drawable:extend{
 	
 	addFrame = function(self, drawable)
 		table.insert(self._animation, drawable)
+	end,
+	
+	getLength = function(self)
+		return #self._animation
 	end
 }
 
