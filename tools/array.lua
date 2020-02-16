acca1ffd4c6b8bfc71290c83891be7070804a31a
Qivex <qivex@web.de>
2020-02-16 15:01:30 +0100

    Moved shortcuts to tools, added array tools (1dim)

---
+++ tools/array.lua
@@ -0,0 +1,18 @@
+-- IMPLEMENTATION
+local to1D = function(width, x, y)
+	return (y-1) * width + x
+end
+
+local from1D = function(width, index)
+	local y = math.ceil(index / width)
+	local x = index - (y-1) * width
+	return x, y
+end
+
+-- EXPORT
+return {
+	index = {
+		to1D = to1D,
+		from1D = from1D
+	}
+}
