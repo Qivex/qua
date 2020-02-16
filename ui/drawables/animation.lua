8ea06ff5b4382649d791c7beee8fe00f6277c24e
Qivex <qivex@web.de>
2020-02-16 12:04:54 +0100

    Don't draw if nothing to draw, keep last frame if animation is over

--- ui/drawables/animation.lua
+++ ui/drawables/animation.lua
@@ -7,12 +7,15 @@ local Animation = Drawable:extend{
 	new = function(self, loop)
 		self._loop = loop
 		self._animation = {}
-		self._current = 1
+		self._current = nil
 	end,
 	
 	draw = function(self, monitor)
-		local drawable = table.remove(self._animation, 1)
-		drawable:draw(monitor)
+		local drawable = table.remove(self._animation, 1) or self._current
+		if drawable then
+			self._current = drawable
+			drawable:draw(monitor)
+		end
 		-- Add frame back to end if looped
 		if self._loop then
 			self:addFrame(drawable)
