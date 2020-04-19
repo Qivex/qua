4c1db4d9c12d2708e19e7a05e0208a6fc9713917
Qivex <qivex@web.de>
2020-04-19 17:35:37 +0200

    Testing different image-handling

--- ui/drawables/image.lua
+++ ui/drawables/image.lua
@@ -7,51 +7,41 @@ local CT = require "qua.tools.color"
 -- IMPLEMENTATION
 local Image = Drawable:extend{	
 	new = function(self, position, size)
+		--[[
 		if type(position) ~= "table" or type(size) ~= "table" then
 			error("Expected {x,y}, {width, height}", 2)
 		end
+		]]
 		self._x, self._y = unpack(position)
 		self._width, self._height = unpack(size)
 		self:clear()
 	end,
 	
 	clear = function(self)
-		-- 1D arrays!
-		self._layers = {
-			bgcol = {},
-			txcol = {},
-			chars = {}
+		local emptyPixel = {
+			bgcol = 2^15,	-- black
+			txcol = 0,		-- white
+			symbol = ""
 		}
+		local pixels = {}
+		for i=1, self._width * self._height do
+			table.insert(pixels, emptyPixel)
+		end
+		self._pixels = pixels
 	end,
 	
 	fromPaint = function(self, path)
 		if fs.exists(path) and not fs.isDir(path) then
 			self:clear()
 			-- Read content
-			local rows = {}
 			local file = fs.open(path, "r")
 			local content = file.readAll() .. "\n"
 			file.close()
 			-- Parse content
-			self:setLayer("bgcol", content)
+			self._pixels = convertMultiline(content)
 		else
 			error("File not found.", 2)
 		end
-	end,
-	
-	setLayer = function(self, layer, data)
-		if layer ~= "bgcol" and layer ~= "txcol" and layer ~= "chars" then
-			error("Undefined layer: Only 'bgcol', 'txcol' or 'chars'", 2)
-		end
-		local pixels = self:convertMultiline(data)
-		if layer ~= "chars" then
-			local colors = {}
-			for index, colorcode in pairs(pixels) do
-				colors[index] = CT.decode(colorcode)
-			end
-			pixels = colors
-		end
-		self._layers[layer] = pixels
 	end,
 	
 	convertMultiline = function(self, multiline)
@@ -59,6 +49,8 @@ local Image = Drawable:extend{
 			error("Expected string.", 2)
 		end
 		local result = {}
+		local index = 1
+		-- Parse text
 		local y = 1
 		for row in multiline:gmatch("(.-)\n") do
 			if y > self._height then
@@ -69,8 +61,8 @@ local Image = Drawable:extend{
 				if x > self._width then
 					break
 				end
-				local index = AT.to1D(self._width, x, y)
-				result[index] = symbol
+				local index = self._width * (y - 1) + x
+				result[index].bgcol = CT.decode(symbol)
 				x = x + 1
 			end
 			y = y + 1
@@ -79,19 +71,21 @@ local Image = Drawable:extend{
 	end,
 	
 	draw = function(self, monitor)
-		local bgcol = self._layers.bgcol
-		local txcol = self._layers.txcol
-		local chars = self._layers.chars
-		-- Only draw on defined background (transparency!)
-		for index in pairs(bgcol) do
-			local x, y = AT.from1D(self._width, index)
+		local x, y = 1, 1
+		for _, pixel in pairs(self._pixels) do
+			if x = self._width then
+				x = 1
+				y = y + 1
+			else
+				x = x + 1
+			end
 			monitor.setCursorPos(
 				x + (self._x - 1),
 				y + (self._y - 1)
 			)
-			monitor.setBackgroundColor(bgcol[index] or colors.black)
-			monitor.setTextColor(txcol[index] or colors.white)
-			monitor.write(chars[index] or " ")
+			monitor.setBackgroundColor(pixel.bgcol)
+			monitor.setTextColor(pixel.txcol)
+			monitor.write(pixel.symbol)
 		end
 	end
 }
