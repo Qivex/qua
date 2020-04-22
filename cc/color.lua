d871789b5402c74e949f09f540a8fbd1a3f08ea1
Qivex <qivex@web.de>
2020-04-22 11:43:19 +0200

    Pulled isValidColor impl. out of self

--- cc/color.lua
+++ cc/color.lua
@@ -15,9 +15,22 @@ for code in PAINT_CHARS:gmatch(".") do
 	color = color * 2
 end
 
+local isValidColor = function(col)
+	-- Color must be number
+	if type(col) ~= "number" then
+		return false
+	end
+	-- Anything inside interval is valid
+	if col >= 1 and col < 65536 then
+		return true
+	else
+		return false
+	end
+end
+
 local Color = Class:extend{
 	new = function(self, col)
-		assert(self.isValidColor(col), "Invalid color!", 3)
+		assert(isValidColor(col), "Invalid color!", 3)
 		self._color = col
 	end,
 	
@@ -25,18 +38,7 @@ local Color = Class:extend{
 		return self._color
 	end,
 	
-	isValidColor = function(col)
-		-- Color must be number
-		if type(col) ~= "number" then
-			return false
-		end
-		-- Anything inside interval is valid
-		if col >= 1 and col < 65536 then
-			return true
-		else
-			return false
-		end
-	end,
+	isValidCover = isValidCover,
 	
 	fromHex = function(symbol)
 		assert(type(symbol) == "string", "Expected string!", 2)
@@ -45,7 +47,7 @@ local Color = Class:extend{
 	end,
 	
 	toHex = function(col)
-		assert(self.isValidColor(col), "Invalid color!", 2)
+		assert(isValidColor(col), "Invalid color!", 2)
 		local base = math.log(col) / math.log(2)	-- simulates 'math.log2(col)'
 		return PAINT_CHARS:sub(base + 1, base + 1)
 	end
