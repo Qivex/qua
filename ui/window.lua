d2d7ae48ce748fa1bacd5ac027127dd600169f33
Qivex <qivex@web.de>
2020-04-30 13:50:47 +0200

    Removed accidental commata

--- ui/window.lua
+++ ui/window.lua
@@ -168,7 +168,7 @@ local Window = Drawable:extend{
 		fm.setBackgroundColor = function(color)
 			validateColor(color)
 			self._cursor:setBackCol(color)
-		end,
+		end
 		
 		fm.setTextScale = function(scale)
 			assert(type(scale) == "number" and scale >= 0.5 and scale <= 5, "Expected number in range 0.5-5!", 2)
